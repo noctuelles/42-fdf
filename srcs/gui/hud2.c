b25f9ae8ae3fd19588fcc680fc4bf3847e538052
Paul Louvel <plouvel@student.42.fr>
2022-01-29 16:22:13 +0100

    reverting back to 2e9a3bed3caec2ffbbdb0d7b0f4788c2cc401675

--- srcs/gui/hud2.c
+++ srcs/gui/hud2.c
@@ -6,11 +6,23 @@
 /*   By: plouvel <plouvel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 15:49:07 by plouvel           #+#    #+#             */
-/*   Updated: 2022/01/29 16:19:03 by plouvel          ###   ########.fr       */
+/*   Updated: 2022/01/29 15:11:37 by plouvel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+void	draw_gradient(t_mlx *mlx)
+{
+	t_vec2d	rect;
+	t_vec2d	pos;
+
+	set_vec2d(&rect, 15, 15);
+	set_vec2d(&pos, 80, 859);
+	draw_full_rect(mlx, rect, pos, mlx->data.gradient[0]);
+	set_vec2d(&pos, 120, 859);
+	draw_full_rect(mlx, rect, pos, mlx->data.gradient[mlx->data.max_z - 1]);
+}
 
 void	draw_compass(t_mlx *mlx)
 {
